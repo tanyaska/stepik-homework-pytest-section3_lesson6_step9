3b7e570404df5d1e1f7079f4fccc2c68af27a760
tanyaska <63187700+tanyaska@users.noreply.github.com>
2020-04-07 19:14:14 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,12 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+def test_guest_should_see_button_add_to_basket(browser):
+    try:
+        browser.get(link)
+        time.sleep(30)
+        browser.find_element_by_css_selector(".btn.btn-lg")
+        found = True
+    except:
+        found = False
+    assert found, "Button 'Add to basket' is not find on page"
